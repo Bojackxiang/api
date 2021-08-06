e7e6f845b280defeb6ef3d9d2fd173aa61c9a3a2
Alex <alex@q-83.com>
2021-08-06 21:21:14 +1000

    修改了如果message class 出错的情况下要抛出的异常

--- src/routers/message.ts
+++ src/routers/message.ts
@@ -8,8 +8,8 @@ messageRouter.post(
   "/create-message",
   async function login(req: Request, res: Response, next: NextFunction) {
     const response = {
-      success: true,
-      message: undefined,
+      success: false,
+      message: '',
     }
     try {
       const message: IMessage | undefined = req.body;
@@ -18,9 +18,11 @@ messageRouter.post(
       }
       console.log(message)
       const addMessageResult = await Message.addMessage(message)
-      console.log(addMessageResult)
+      if(!addMessageResult.success){
+        throw new Error(addMessageResult.message)
+      }
 
-
+      // response to the front end 
       res.json(response)
     } catch (err) {
       response.success = false
