f74b21356cd92721199ff560b587f3b84782dc45
Alex <alex@q-83.com>
2021-08-24 17:53:06 +1000

    rest client 的学习和测试

--- src/routers/test.ts
+++ src/routers/test.ts
@@ -5,10 +5,29 @@ const testRouter = express.Router();
  * 这个 function 主要是为了测试一些新的功能
  * 
  */ 
- testRouter.get('/test', (req: Request, res: Response) => {
-   console.log('---');
+ testRouter.get('/general-error', (req: Request, res: Response) => {
     const response = JSONResult.error()
     res.json(response)
  })
+
+ testRouter.get('/auth-error', (req: Request, res: Response) => {
+   const response = JSONResult.authError()
+   res.json(response)
+})
+
+testRouter.get('/not-found-error', (req: Request, res: Response) => {
+   const response = JSONResult.notFoundError()
+   res.json(response)
+})
+
+testRouter.get('/general-error', (req: Request, res: Response) => {
+   const response = JSONResult.error()
+   res.json(response)
+})
+
+testRouter.get('/ok', (req: Request, res: Response) => {
+   const response = JSONResult.ok()
+   res.json(response)
+})
 
  export default testRouter;
