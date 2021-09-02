b0a54970d3b852410ecdc3ba460b01d16a2def34
alex <batmandocode@gmail.com>
2021-09-02 10:59:58 +1000

    添加了 pdf 文件下载的 snippet

---
+++ src/service/file-download.ts
@@ -0,0 +1,11 @@
+import axios from axios;
+
+export default async function downloadPDF(){
+
+    const response = await axios.get('http://www.africau.edu/images/default/sample.pdf');
+    // downloadFile should shape [ Buffer <12 ....>]
+    Buffer.concat(response)
+
+    return response;
+
+}
